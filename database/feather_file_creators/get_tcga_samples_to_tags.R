de2a84703a6a29a97b060ddbaf2f5010407e2037
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-01-13 14:26:56 -0800

    Script to build samples to tags demo feather files by study.

---
+++ database/feather_file_creators/get_tcga_samples_to_tags.R
@@ -0,0 +1,68 @@
+source("../load_dependencies.R")
+
+.GlobalEnv$load_dependencies()
+
+rm(load_dependencies, pos = ".GlobalEnv")
+
+# The database connection.
+source("../connect_to_db.R", chdir = TRUE)
+
+# Create a global variable to hold the pool DB connection.
+.GlobalEnv$pool <- .GlobalEnv$connect_to_db()
+
+cat(crayon::green("Created DB connection."), fill = TRUE)
+
+get_tcga_samples_to_tags <- function(study) {
+  current_pool <- pool::poolCheckout(.GlobalEnv$pool)
+
+  samples_to_tags <- current_pool %>%
+    dplyr::tbl("samples_to_tags") %>%
+    dplyr::as_tibble() %>%
+    dplyr::right_join(
+      current_pool %>%
+        dplyr::tbl("tags_to_tags") %>%
+        dplyr::as_tibble() %>%
+        dplyr::right_join(
+          current_pool %>%
+            dplyr::tbl("tags") %>%
+            dplyr::as_tibble(),
+          by = c("related_tag_id" = "id")
+        ) %>%
+        dplyr::filter(name == study),
+      by = "tag_id"
+    ) %>%
+    dplyr::distinct(sample_id, tag_id)
+
+  pool::poolReturn(current_pool)
+
+  return(samples_to_tags)
+}
+
+tcga_study_samples_to_tags <- "TCGA_Study" %>%
+  get_tcga_samples_to_tags %>%
+  feather::write_feather("../../feather_files/samples/tcga_study_samples_to_tags.feather")
+
+tcga_subtype_samples_to_tags <- "TCGA_Subtype" %>%
+  get_tcga_samples_to_tags %>%
+  feather::write_feather("../../feather_files/samples/tcga_subtype_samples_to_tags.feather")
+
+immune_subtype_samples_to_tags <- "Immune_Subtype" %>%
+  get_tcga_samples_to_tags %>%
+  feather::write_feather("../../feather_files/samples/immune_subtype_samples_to_tags.feather")
+
+# Close the database connection.
+pool::poolClose(.GlobalEnv$pool)
+cat(crayon::green("Closed DB connection."), fill = TRUE)
+
+### Clean up ###
+# Data
+rm(pool, pos = ".GlobalEnv")
+# rm(tcga_study_samples_to_tags)
+# rm(tcga_subtype_samples_to_tags)
+# rm(immune_subtype_samples_to_tags)
+
+# Functions
+rm(get_tcga_samples_to_tags, pos = ".GlobalEnv")
+
+cat("Cleaned up.", fill = TRUE)
+gc()
