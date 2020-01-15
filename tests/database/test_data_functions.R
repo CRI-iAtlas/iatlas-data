c4623fccad16da529525ebe6f871aed5a99232ce
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-01-15 13:54:18 -0800

    Explicitly namespacing testthat functions.

--- tests/database/test_data_functions.R
+++ tests/database/test_data_functions.R
@@ -22,15 +22,15 @@ source("../../database/data_functions.R", chdir = TRUE)
 
 # filter_na
 testthat::test_that("filter_na returns the value when the passed value is NOT NA.", {
-  testthat::expect_that(filter_na(c(14)), is_identical_to(14))
-  testthat::expect_that(filter_na(14), is_identical_to(14))
+  testthat::expect_that(filter_na(c(14)), testthat::is_identical_to(14))
+  testthat::expect_that(filter_na(14), testthat::is_identical_to(14))
 })
 testthat::test_that("filter_na returns the value when the passed value is combined with an NA.", {
-  testthat::expect_that(filter_na(c(14, NA)), is_identical_to(14))
+  testthat::expect_that(filter_na(c(14, NA)), testthat::is_identical_to(14))
 })
 # testthat::test_that("filter_na returns NA when there is no passed value or the passed value is NA.", {
 #   cat("value:", filter_na(NA), fill = TRUE, sep = " ")
-#   testthat::expect_that(filter_na(NA), is_identical_to(NA %>% as.character))
+#   testthat::expect_that(filter_na(NA), testthat::is_identical_to(NA %>% as.character))
 # })
 
 # get_tag_column_names
@@ -46,47 +46,47 @@ testthat::test_that("filter_na returns the value when the passed value is combin
 testthat::test_that("get_tag_column_names returns a character vector of column names that beging with 'tag'.", {
   data_frame <- feather::read_feather("../test_data/get_tag_column_names.feather")
   result <- get_tag_column_names(data_frame)
-  testthat::expect_that(result[1], is_identical_to("tag"))
-  testthat::expect_that(result[2], is_identical_to("tag.01"))
-  testthat::expect_that(result[3], is_identical_to("tag.11"))
-  testthat::expect_that(result[4], is_identical_to("tag.x"))
+  testthat::expect_that(result[1], testthat::is_identical_to("tag"))
+  testthat::expect_that(result[2], testthat::is_identical_to("tag.01"))
+  testthat::expect_that(result[3], testthat::is_identical_to("tag.11"))
+  testthat::expect_that(result[4], testthat::is_identical_to("tag.x"))
 })
 testthat::test_that("get_tag_column_names returns NA when an empty data frame, NULL, or NA is passed.", {
   data_frame <- dplyr::tibble()
-  testthat::expect_that(get_tag_column_names(data_frame), is_identical_to(NA))
-  testthat::expect_that(get_tag_column_names(NULL), is_identical_to(NA))
-  testthat::expect_that(get_tag_column_names(NA), is_identical_to(NA))
+  testthat::expect_that(get_tag_column_names(data_frame), testthat::is_identical_to(NA))
+  testthat::expect_that(get_tag_column_names(NULL), testthat::is_identical_to(NA))
+  testthat::expect_that(get_tag_column_names(NA), testthat::is_identical_to(NA))
 })
 
 # is_df_empty
 testthat::test_that("is_df_empty returns FALSE when a valid non-empty dataframe or tibble is passed.", {
-  testthat::expect_that(is_df_empty(cars), is_identical_to(FALSE))
-  testthat::expect_that(is_df_empty(cars %>% dplyr::as_tibble()), is_identical_to(FALSE))
+  testthat::expect_that(is_df_empty(cars), testthat::is_identical_to(FALSE))
+  testthat::expect_that(is_df_empty(cars %>% dplyr::as_tibble()), testthat::is_identical_to(FALSE))
 })
 testthat::test_that("is_df_empty returns TRUE when an empty dataframe, empty tibble, NA, NULL, or no data frame is passed.", {
-  testthat::expect_that(is_df_empty(data.frame()), is_identical_to(TRUE))
-  testthat::expect_that(is_df_empty(NA), is_identical_to(TRUE))
-  testthat::expect_that(is_df_empty(NULL), is_identical_to(TRUE))
-  testthat::expect_that(is_df_empty(), is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(data.frame()), testthat::is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(NA), testthat::is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(NULL), testthat::is_identical_to(TRUE))
+  testthat::expect_that(is_df_empty(), testthat::is_identical_to(TRUE))
 })
 
 # link_to_references
 testthat::test_that("link_to_references returns NA when no value present.", {
-  testthat::expect_that(link_to_references(NA), is_identical_to(NA))
+  testthat::expect_that(link_to_references(NA), testthat::is_identical_to(NA))
 })
 testthat::test_that("link_to_references returns NA when URL value is 'NA'.", {
   link <- '<a href="NA">NA</a>'
-  testthat::expect_that(link_to_references(link), is_identical_to(NA))
+  testthat::expect_that(link_to_references(link), testthat::is_identical_to(NA))
 })
 testthat::test_that("link_to_references returns NA when there is no URL.", {
   link <- '<a href>NA</a>'
-  testthat::expect_that(link_to_references(link), is_identical_to(NA))
+  testthat::expect_that(link_to_references(link), testthat::is_identical_to(NA))
 })
 testthat::test_that("link_to_references returns a url in curly braces.", {
   url <- 'http://someplace.com?query=yes#pow'
   link <- paste0('<a href="', url, '"></a>', sep = "")
   expected <- paste0('{', url, '}', sep = "")
-  testthat::expect_that(link_to_references(link), is_identical_to(expected))
+  testthat::expect_that(link_to_references(link), testthat::is_identical_to(expected))
 })
 
 # switch_value
@@ -94,11 +94,11 @@ testthat::test_that("switch_value returns NA when no value present.", {
   reference <- dplyr::tibble(gene = c("1", "2", "3"), scoobs = c(NA, NA, NA))
   object <- dplyr::tibble(gene = c("", "", ""), scoobs = c("4", "5", "6"))
 
-  testthat::expect_that(switch_value(reference[1,], "gene", "scoobs", object), is_identical_to(NA))
+  testthat::expect_that(switch_value(reference[1,], "gene", "scoobs", object), testthat::is_identical_to(NA))
 })
 testthat::test_that("switch_value returns the value from the second object.", {
   reference <- dplyr::tibble(gene = c("1", "2", "3"), scoobs = c(NA, NA, NA))
   object <- dplyr::tibble(gene = c("1", "", ""), scoobs = c("4", "5", "6"))
 
-  testthat::expect_that(switch_value(reference[1,], "gene", "scoobs", object), is_identical_to("4"))
+  testthat::expect_that(switch_value(reference[1,], "gene", "scoobs", object), testthat::is_identical_to("4"))
 })
