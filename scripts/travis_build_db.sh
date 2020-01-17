313716745d6f6063110eb6e5868c49a34c4a8d13
Thatcher Hubbard <26883046+thatcherhubbard@users.noreply.github.com>
2020-01-17 15:15:28 -0800

    Changing psql and pg_dump calls

--- scripts/travis_build_db.sh
+++ scripts/travis_build_db.sh
@@ -5,20 +5,20 @@ GREEN="\033[0;32m"
 # No Color
 NC='\033[0m'
 
-BASE_URL='postgres://postgres:T!tus0s30@sage-test.cau5la50px0r.us-west-2.rds.amazonaws.com'
+STAGING_DB_HOST=sage-test.cau5la50px0r.us-west-2.rds.amazonaws.com
 
 TARGET_DB=iatlas_staging
 NEW_DB=${TARGET_DB}_`date +"%s"`
 
 # Make the new temporary DB
-echo "${GREEN}Creating new target DB @ ${BASE_URL}/postgres${NC}"
-psql "${BASE_URL}/postgres" -c"CREATE DATABASE ${NEW_DB}"
-echo "${GREEN}Restoring to DB @ ${BASE_URL}/${TARGET_DB}${NC}"
-pg_restore -d ${BASE_URL}/${NEW_DB} /tmp/iatlas_dev.pgdata
-echo "${GREEN}Swapping new and target DB @ ${BASE_URL}/postgres${NC}"
-psql "${BASE_URL}/postgres" -c"ALTER DATABASE ${TARGET_DB} RENAME TO ${TARGET_DB}_old"
-psql "${BASE_URL}/postgres" -c"ALTER DATABASE ${NEW_DB} RENAME TO ${TARGET_DB}"
-psql "${BASE_URL}/postgres" -c"DROP DATABASE ${NEW_DB}"
+>&2 echo -e "${GREEN}Creating new target DB @ ${BASE_URL}/postgres${NC}"
+PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST  -c"CREATE DATABASE ${NEW_DB}"
+>&2 echo -e "${GREEN}Restoring to DB @ ${BASE_URL}/${TARGET_DB}${NC}"
+#pg_restore -d ${NEW_DB} -h /tmp/iatlas_dev.pgdata
+>&2 -e echo "${GREEN}Swapping new and target DB @ ${BASE_URL}/postgres${NC}"
+PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST -c"ALTER DATABASE ${TARGET_DB} RENAME TO ${TARGET_DB}_old"
+PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST -c"ALTER DATABASE ${NEW_DB} RENAME TO ${TARGET_DB}"
+PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST -c"DROP DATABASE ${NEW_DB}"
 
 
 
