7410aab29a03b694d465e97fcd766d38c52dcfe8
Thatcher Hubbard <26883046+thatcherhubbard@users.noreply.github.com>
2020-01-17 15:21:50 -0800

    Changes to debug DB connection

--- scripts/travis_build_db.sh
+++ scripts/travis_build_db.sh
@@ -11,11 +11,13 @@ TARGET_DB=iatlas_staging
 NEW_DB=${TARGET_DB}_`date +"%s"`
 
 # Make the new temporary DB
+psql -V
+which psql
 >&2 echo -e "${GREEN}Creating new target DB @ ${BASE_URL}/postgres${NC}"
 PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST  -c"CREATE DATABASE ${NEW_DB}"
 >&2 echo -e "${GREEN}Restoring to DB @ ${BASE_URL}/${TARGET_DB}${NC}"
 #pg_restore -d ${NEW_DB} -h /tmp/iatlas_dev.pgdata
->&2 -e echo "${GREEN}Swapping new and target DB @ ${BASE_URL}/postgres${NC}"
+>&2 echo -e "${GREEN}Swapping new and target DB @ ${BASE_URL}/postgres${NC}"
 PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST -c"ALTER DATABASE ${TARGET_DB} RENAME TO ${TARGET_DB}_old"
 PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST -c"ALTER DATABASE ${NEW_DB} RENAME TO ${TARGET_DB}"
 PGPASSWORD='T!tus0s30' psql -U postgres -h $STAGING_DB_HOST -c"DROP DATABASE ${NEW_DB}"
