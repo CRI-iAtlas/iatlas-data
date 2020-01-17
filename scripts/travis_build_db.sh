efb058190dfb5a596ebd2e17bed9e355a5d81b4a
Thatcher Hubbard <26883046+thatcherhubbard@users.noreply.github.com>
2020-01-17 11:58:23 -0800

    Fixing bad '-f' passed to pg_restore that causes the command to fail

--- scripts/travis_build_db.sh
+++ scripts/travis_build_db.sh
@@ -11,7 +11,7 @@ NEW_DB=${TARGET_DB}_`date +"%s"`
 BASE_URL='postgres://postgres:T!tus0s30@sage-test.cau5la50px0r.us-west-2.rds.amazonaws.com'
 # Make the new temporary DB
 psql "${BASE_URL}/postgres" -c"CREATE DATABASE ${NEW_DB}"
-pg_restore -d ${BASE_URL}/${NEW_DB} -f /tmp/iatlas_dev.pgdata
+pg_restore -d ${BASE_URL}/${NEW_DB} /tmp/iatlas_dev.pgdata
 psql "${BASE_URL}/postgres" -c"ALTER DATABASE ${TARGET_DB} RENAME TO ${TARGET_DB}_old"
 psql "${BASE_URL}/postgres" -c"ALTER DATABASE ${NEW_DB} RENAME TO ${TARGET_DB}"
 psql "${BASE_URL}/${NEW_DB}" -c"DROP DATABASE ${NEW_DB}"
