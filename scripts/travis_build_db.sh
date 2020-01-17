fbe8fe4ba84428114648251a17b24da723e76a12
Thatcher Hubbard <26883046+thatcherhubbard@users.noreply.github.com>
2020-01-17 14:44:15 -0800

    Added status output messages to travis_build_db.sh

--- scripts/travis_build_db.sh
+++ scripts/travis_build_db.sh
@@ -5,16 +5,20 @@ GREEN="\033[0;32m"
 # No Color
 NC='\033[0m'
 
-TARGET_DB=iatlas_dev
+BASE_URL='postgres://postgres:T!tus0s30@sage-test.cau5la50px0r.us-west-2.rds.amazonaws.com'
+
+TARGET_DB=iatlas_staging
 NEW_DB=${TARGET_DB}_`date +"%s"`
 
-BASE_URL='postgres://postgres:T!tus0s30@sage-test.cau5la50px0r.us-west-2.rds.amazonaws.com'
 # Make the new temporary DB
+echo "${GREEN}Creating new target DB @ ${BASE_URL}/postgres${NC}"
 psql "${BASE_URL}/postgres" -c"CREATE DATABASE ${NEW_DB}"
+echo "${GREEN}Restoring to DB @ ${BASE_URL}/${TARGET_DB}${NC}"
 pg_restore -d ${BASE_URL}/${NEW_DB} /tmp/iatlas_dev.pgdata
+echo "${GREEN}Swapping new and target DB @ ${BASE_URL}/postgres${NC}"
 psql "${BASE_URL}/postgres" -c"ALTER DATABASE ${TARGET_DB} RENAME TO ${TARGET_DB}_old"
 psql "${BASE_URL}/postgres" -c"ALTER DATABASE ${NEW_DB} RENAME TO ${TARGET_DB}"
-psql "${BASE_URL}/${NEW_DB}" -c"DROP DATABASE ${NEW_DB}"
+psql "${BASE_URL}/postgres" -c"DROP DATABASE ${NEW_DB}"
 
 
 
