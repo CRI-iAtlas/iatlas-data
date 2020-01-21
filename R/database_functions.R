3c488ae45e12e64c522b6b42aa6af719fd13622a
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-01-21 14:29:16 -0800

    Removed annoying extra quote.

--- R/database_functions.R
+++ R/database_functions.R
@@ -8,7 +8,7 @@ delete_rows <- function(table_name) {
 }
 
 read_table <- function(table_name) {
-  tictoc::tic(paste0("Time taken to read from the `", table_name, "`` table in the DB"))
+  tictoc::tic(paste0("Time taken to read from the `", table_name, "` table in the DB"))
   current_pool <- pool::poolCheckout(.GlobalEnv$pool)
   result <- pool::dbReadTable(current_pool, table_name)
   pool::poolReturn(current_pool)
@@ -30,7 +30,7 @@ read_table <- function(table_name) {
 # }
 
 write_table_ts <- function(df, table_name) {
-  tictoc::tic(paste0("Time taken to write to the `", table_name, "`` table in the DB"))
+  tictoc::tic(paste0("Time taken to write to the `", table_name, "` table in the DB"))
   result <- pool::poolWithTransaction(.GlobalEnv$pool, function(conn) {
     # Disable all table indexes.
     conn %>% pool::dbExecute(paste0(
