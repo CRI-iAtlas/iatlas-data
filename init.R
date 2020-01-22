75fd13105a0050ce2c1edaf51c750b93bd5988a9
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-01-22 14:09:32 -0800

    build script can now be resumed or can skip initial steps

--- init.R
+++ init.R
@@ -4,18 +4,42 @@ source("R/load_dependencies.R")
 
 rm(load_dependencies, pos = ".GlobalEnv")
 
-#' @export
-build_iatlas_db <- function(env = "dev", reset = NULL, show_gc_info = FALSE) {
+#' build_iatlas_db
+#'
+#' Build the full iAtlas database from source feather-files
+#'
+#' @param env
+#' @param reset "reset" or "create" or NULL
+#' @param resume_at = NULL or step-name-string - will skip all steps until the specified step, which will be executed as well as all following steps
+#' @param stop_after = NULL or step-name-string - will stop executing AFTER executing the specified step. Will not execute any more steps.
+#' @return nothing
+build_iatlas_db <- function(env = "dev", reset = NULL, show_gc_info = FALSE, resume_at = NULL, stop_after = NULL) {
 
-  run_build_script <- function(script_name, param) {
-    source(paste0("R/",script_name,".R"))$value(param)
+  present <- function (a) {!is.na(a) && !is.null(a)}
+  option_equal <- function (a, b) {present(a) && present(b) && a == b}
+  running_is_on <- is.null(resume_at)
+
+  run_build_script <- function(script_name, ...) {
+    if (option_equal(resume_at, script_name)) running_is_on <<- TRUE;
+    if (running_is_on) {
+      cat(crayon::green(paste0("before build step: ", script_name)), fill = TRUE)
+
+      tryCatch({
+        source(paste0("R/",script_name,".R"))$value(...)
+      }, error = function(e) {
+        cat(crayon::magenta(crayon::bold(paste0("resume here with option: resume_at = '", script_name, "'"))))
+        running_is_on <<- FALSE
+        stop(e)
+      })
+      cat(crayon::green(paste0("after build step: ", script_name)), fill = TRUE)
+    } else {
+      cat(crayon::green(paste0("Skipping '", script_name, "' (as requested by resume_at or stop_after options)" )), fill = TRUE)
+    }
+    if (option_equal(stop_after, script_name)) {
+      cat(crayon::bold(crayon::green(paste0("Stopping after: '", script_name, "' (as requested by stop_after = '",script_name, "')"))), fill = TRUE)
+      running_is_on <<- FALSE;
+    }
   }
-
-  # Make the create_db function available.
-  source("R/create_db.R", chdir = TRUE)
-
-  # Reset the database so new data is not corrupted by any old data.
-  .GlobalEnv$create_db(env, reset)
 
   # Make the custom data functions available.
   source("R/data_functions.R", chdir = TRUE)
@@ -23,11 +47,14 @@ build_iatlas_db <- function(env = "dev", reset = NULL, show_gc_info = FALSE) {
   # Make the custom database functions available.
   source("R/database_functions.R", chdir = TRUE)
 
-  # Show garbage collection info
-  gcinfo(show_gc_info)
-
   # The database connection.
   source("R/connect_to_db.R", chdir = TRUE)
+
+  # Reset the database so new data is not corrupted by any old data.
+  run_build_script("create_db", env, reset)
+
+  # Show garbage collection info
+  gcinfo(show_gc_info)
 
   # Create a global variable to hold the pool DB connection.
   .GlobalEnv$pool <- .GlobalEnv$connect_to_db()
@@ -38,7 +65,7 @@ build_iatlas_db <- function(env = "dev", reset = NULL, show_gc_info = FALSE) {
   run_build_script("build_tags_tables", "feather_files/SQLite_data/groups.feather")
   run_build_script("build_gene_tables", "feather_files")
 
-  source("database/build_samples_tables.R", chdir = TRUE)
+  # source("database/build_samples_tables.R", chdir = TRUE)
 
   # source("database/build_driver_results_tables.R", chdir = TRUE)
   #
@@ -55,7 +82,6 @@ build_iatlas_db <- function(env = "dev", reset = NULL, show_gc_info = FALSE) {
 
   # Functions
   rm(connect_to_db, pos = ".GlobalEnv")
-  rm(create_db, pos = ".GlobalEnv")
   rm(delete_rows, pos = ".GlobalEnv")
   rm(driver_results_label_to_hgnc, pos = ".GlobalEnv")
   rm(filter_na, pos = ".GlobalEnv")
@@ -72,4 +98,5 @@ build_iatlas_db <- function(env = "dev", reset = NULL, show_gc_info = FALSE) {
 
   # Don't show garbage collection details any longer.
   gcinfo(FALSE)
+  return(NA);
 }
