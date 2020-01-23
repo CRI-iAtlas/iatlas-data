f54814c5e4520e8cc1c9e5f5e94282d624df0130
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-01-22 17:25:52 -0800

    FIX - basic DB tests working

--- tests/testthat/test_build_features_table.R
+++ tests/testthat/test_build_features_table.R
@@ -2,24 +2,26 @@
   library("testthat")
   library('feather')
 
-  pool <- null
+  .GlobalEnv$pool <- NULL
 
-  setup(source("../../.Rprofile.d/ENV=test.R"))
+  setup(source(paste0(rprojroot::find_root("DESCRIPTION"), "/.Rprofile.d/ENV=test.R")))
 
   test_that("create_db", {
     create_db("test", "reset")
   })
 
   test_that("connect_db", {
-    pool <<- connect_to_db()
+    .GlobalEnv$pool <- connect_to_db()
   })
 
-    # build_references
+  # build_references
   test_that("build_features_table", {
     expect_equal(.GlobalEnv$DB_NAME, "iatlas_shiny_test")
     # build_features_table("../../feather_files/SQLite_data/features.feather")
   })
 
-  teardown(pool::poolClose(pool))
+  teardown(pool::poolClose(.GlobalEnv$pool))
+  teardown(rm(pool, pos = ".GlobalEnv"))
 
+  teardown(source(paste0(rprojroot::find_root("DESCRIPTION"), "/.Rprofile.d/ENV=dev.R")))
 })()
