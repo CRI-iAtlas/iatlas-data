ff568e32e3f356c6e69810ffd1d23ed4267196d3
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-01-29 11:41:42 -0800

    NEW - read_iatlas_data_file now accepts * and ** for globing

--- R/load_all_samples.R
+++ R/load_all_samples.R
@@ -3,15 +3,8 @@ load_all_samples <- function(feather_file_folder) {
   on.exit(cat(crayon::blue("Imported feather files for samples and combined all the sample data."), fill = TRUE))
   feature_values_long <- read_iatlas_data_file(feather_file_folder,"SQLite_data/feature_values_long.feather")
   all_samples <- dplyr::bind_rows(
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/driver_mutations1.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/driver_mutations2.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/driver_mutations3.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/driver_mutations4.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/driver_mutations5.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/io_target_expr1.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/io_target_expr2.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/io_target_expr3.feather"),
-      read_iatlas_data_file(feather_file_folder,"SQLite_data/io_target_expr4.feather"),
+      read_iatlas_data_file(feather_file_folder,"SQLite_data/driver_mutations*.feather"),
+      read_iatlas_data_file(feather_file_folder,"SQLite_data/io_target_expr*.feather"),
       read_iatlas_data_file(feather_file_folder,"SQLite_data/immunomodulator_expr.feather")
     ) %>%
     dplyr::rename(rna_seq_expr = value) %>%
