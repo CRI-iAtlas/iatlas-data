c14360a8c57326e0b2d17d031bdf9516f094eaba
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-01-31 10:56:48 -0800

    Feather file creator "get_driver_results" works as part of iatlas.data namespace.

--- R/feather_file_creator-get_driver_results.R
+++ R/feather_file_creator-get_driver_results.R
@@ -6,30 +6,35 @@ get_driver_results <- function() {
   get_driver_results <- function() {
     current_pool <- pool::poolCheckout(.GlobalEnv$pool)
 
-    driver_results <- current_pool %>%
-      dplyr::tbl("driver_results") %>%
-      dplyr::left_join(
-        current_pool %>%
-          dplyr::tbl("features") %>%
-          dplyr::select(id, name) %>%
-          dplyr::rename_at("name", ~("feature")),
+    # Get the initial values from the driver_results table.
+    driver_results <- current_pool %>% dplyr::tbl("driver_results")
+
+    # Get features related to the driver results.
+    driver_results <- driver_results %>% dplyr::left_join(
+        current_pool %>% dplyr::tbl("features") %>%
+          dplyr::select(id, feature = name),
         by = c("feature_id" = "id")
-      ) %>%
-      dplyr::left_join(
-        current_pool %>%
-          dplyr::tbl("genes") %>%
+      )
+
+    # Get genes related to the driver results.
+    driver_results <- driver_results %>% dplyr::left_join(
+        current_pool %>% dplyr::tbl("genes") %>%
           dplyr::select(id, hgnc),
         by = c("gene_id" = "id")
-      ) %>%
-      dplyr::left_join(
-        current_pool %>%
-          dplyr::tbl("tags") %>%
-          dplyr::select(id, name) %>%
-          dplyr::rename_at("name", ~("tag")),
+      )
+
+    # Get tags related to the driver results.
+    driver_results <- driver_results %>% dplyr::left_join(
+        current_pool %>% dplyr::tbl("tags") %>%
+          dplyr::select(id, tag = name),
         by = c("tag_id" = "id")
-      ) %>%
-      dplyr::distinct(feature, hgnc, tag, p_value, fold_change, log10_p_value, log10_fold_change, n_wt, n_mut) %>%
-      dplyr::as_tibble()
+      )
+
+    # Clean up the data set.
+    driver_results <- driver_results %>% dplyr::distinct(feature, hgnc, tag, p_value, fold_change, log10_p_value, log10_fold_change, n_wt, n_mut)
+
+    # Execute the query and return a tibble.
+    driver_results <- driver_results %>% dplyr::as_tibble()
 
     pool::poolReturn(current_pool)
 
