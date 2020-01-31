44b1277b069bec14fa49beb2ac113850feb44bbf
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-01-31 14:34:11 -0800

    Feather file creator "get_samples_to_tags_by_study" works as part of iatlas.data namespace.

--- R/feather_file_creator-get_samples_to_tags_by_study.R
+++ R/feather_file_creator-get_samples_to_tags_by_study.R
@@ -3,40 +3,55 @@ get_samples_to_tags_by_study <- function() {
   .GlobalEnv$pool <- iatlas.data::connect_to_db()
   cat(crayon::green("Created DB connection."), fill = TRUE)
 
+  cat_samples_to_tags_status <- function(message) {
+    cat(crayon::cyan(paste0(" - ", message)), fill = TRUE)
+  }
+
   get_samples_to_tags <- function(study) {
     current_pool <- pool::poolCheckout(.GlobalEnv$pool)
 
-    samples_to_tags <- current_pool %>%
-      dplyr::tbl("samples_to_tags") %>%
-      dplyr::right_join(
-        current_pool %>%
-          dplyr::tbl("tags_to_tags") %>%
-          dplyr::right_join(
-            current_pool %>%
-              dplyr::tbl("tags") %>%
-              dplyr::select(id, name) %>%
-              dplyr::rename_at("name", ~("study_name")),
-            by = c("related_tag_id" = "id")
-          ) %>%
-          dplyr::filter(study_name == study),
-        by = "tag_id"
-      ) %>%
-      dplyr::left_join(
-        current_pool %>%
-          dplyr::tbl("samples") %>%
-          dplyr::select(id, name) %>%
-          dplyr::rename_at("name", ~("sample")),
-        by = c("related_tag_id" = "id")
-      ) %>%
-      dplyr::left_join(
-        current_pool %>%
-          dplyr::tbl("tags") %>%
-          dplyr::select(id, name) %>%
-          dplyr::rename_at("name", ~("tag")),
-        by = c("tag_id" = "id")
-      ) %>%
+    cat(crayon::magenta(paste0("Get samples_to_tags by `", study, "`")), fill = TRUE)
+
+    cat_samples_to_tags_status("Get the initial values from the samples_to_tags table.")
+    samples_to_tags <- current_pool %>% dplyr::tbl("samples_to_tags")
+
+    cat_samples_to_tags_status("Get the tag names for the samples by tag id.")
+    samples_to_tags <- samples_to_tags %>% dplyr::left_join(
+      current_pool %>% dplyr::tbl("tags") %>%
+        dplyr::select(id, tag = name),
+      by = c("tag_id" = "id")
+    )
+
+    cat_samples_to_tags_status("Get tag ids related to the tags :)")
+    samples_to_tags <- samples_to_tags %>% dplyr::right_join(
+      current_pool %>% dplyr::tbl("tags_to_tags"),
+      by = "tag_id"
+    )
+
+    cat_samples_to_tags_status("Get the related tag names for the samples by related tag id.")
+    samples_to_tags <- samples_to_tags %>% dplyr::left_join(
+      current_pool %>% dplyr::tbl("tags") %>%
+        dplyr::select(id, related_tag = name),
+      by = c("related_tag_id" = "id")
+    )
+
+    cat_samples_to_tags_status("Filter the data set to tags related to the passed study.")
+    samples_to_tags <- samples_to_tags %>% dplyr::filter(tag == study | related_tag == study)
+
+    cat_samples_to_tags_status("Get the sample names.")
+    samples_to_tags <- samples_to_tags %>% dplyr::left_join(
+      current_pool %>% dplyr::tbl("samples") %>%
+        dplyr::select(id, sample = name),
+      by = c("sample_id" = "id")
+    )
+
+    cat_samples_to_tags_status("Clean up the data set.")
+    samples_to_tags <- samples_to_tags %>%
       dplyr::distinct(sample, tag) %>%
-      dplyr::as_tibble()
+      dplyr::arrange(sample, tag)
+
+    cat_samples_to_tags_status("Execute the query and return a tibble.")
+    samples_to_tags <- samples_to_tags %>% dplyr::as_tibble()
 
     pool::poolReturn(current_pool)
 
@@ -63,9 +78,9 @@ get_samples_to_tags_by_study <- function() {
   ### Clean up ###
   # Data
   rm(pool, pos = ".GlobalEnv")
-  rm(tcga_study_samples_to_tags, pos = ".GlobalEnv")
-  rm(tcga_subtype_samples_to_tags, pos = ".GlobalEnv")
-  rm(immune_subtype_samples_to_tags, pos = ".GlobalEnv")
+  # rm(tcga_study_samples_to_tags, pos = ".GlobalEnv")
+  # rm(tcga_subtype_samples_to_tags, pos = ".GlobalEnv")
+  # rm(immune_subtype_samples_to_tags, pos = ".GlobalEnv")
   cat("Cleaned up.", fill = TRUE)
   gc()
 }
