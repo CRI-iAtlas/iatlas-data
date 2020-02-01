5e99eb2d48dc7c52499de2dd998b3f565599b671
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-01-31 16:45:09 -0800

    REFACTOR - build_samples_tables has been moved into build_iatlas_db so we can skip directly to the sub-steps

--- R/build_iatlast_db.R
+++ R/build_iatlast_db.R
@@ -17,6 +17,9 @@ build_iatlas_db <- function(env = "dev", reset = "reset", show_gc_info = FALSE, 
   running_is_on <- is.null(resume_at)
   stopped <- FALSE
 
+  num_skippable_steps <- 12 # search this file and count for run_skippable_function calls
+  skippable_step_count <- 1
+
   tictoc::tic(paste0("Time taken to build iAtlas DB"))
 
   run_skippable_function <- function(f, ...) {
@@ -24,7 +27,8 @@ build_iatlas_db <- function(env = "dev", reset = "reset", show_gc_info = FALSE, 
     if (option_equal(resume_at, function_name)) running_is_on <<- TRUE;
     if (running_is_on) {
       cat(crayon::green("\n--------------------------------------------------------------------------------"), fill = TRUE)
-      cat(crayon::green(paste0("START: ", function_name)), fill = TRUE)
+      cat(crayon::green(paste0("START: ", function_name, " (build_iatlas_db step ", skippable_step_count, "/", num_skippable_steps, ")")), fill = TRUE)
+      skippable_step_count <<- skippable_step_count + 1
 
       tryCatch({
         .GlobalEnv$resume_at <- function_name
@@ -65,27 +69,41 @@ build_iatlas_db <- function(env = "dev", reset = "reset", show_gc_info = FALSE, 
   run_skippable_function(build_tags_tables,           feather_file_folder)
   run_skippable_function(build_genes_tables,          feather_file_folder)
 
-  if (FALSE) {
-    all_samples <- NULL
-    get_all_samples <- function () {
-      if (is.null(all_samples)) {
-        all_samples <<- load_all_samples(feather_file_folder)
-      }
-      all_samples
-    }
-    all_samples <- get_all_samples()
+  # before build-samples-tables ---------------------------------------------------
 
-    run_skippable_function(build_samples_tables,         feather_file_folder, get_all_samples)
+  # read genes table, rna-sequence-expression and raw samples ---------------------------------------------------
+  genes <- iatlas.data::read_table("genes") %>% dplyr::as_tibble() %>% dplyr::select(id, hgnc)
+  rna_seq_expr_matrix <- load_rna_seq_expr(feather_file_folder, genes)
+  all_samples <- load_all_samples(feather_file_folder)
 
-    samples <- iatlas.data::read_table("samples") %>% dplyr::as_tibble()
+  run_skippable_function(build_patients_table, feather_file_folder, all_samples, rna_seq_expr_matrix)
 
-    run_skippable_function(build_samples_to_tags_table,     feather_file_folder, get_all_samples, samples)
-    run_skippable_function(build_samples_to_features_table, feather_file_folder, get_all_samples, samples)
+  # read-back patients and add patient_id to samples ---------------------------------------------------
+  cat(crayon::magenta("Add patient_id to samples data."), fill = TRUE)
+  patients <- iatlas.data::read_table("patients") %>% dplyr::select(patient_id = id, sample = barcode)
+  all_samples_with_patient_ids <- all_samples %>% dplyr::left_join(patients, by = "sample")
+  cat(crayon::blue("Added patient_id to samples data."), fill = TRUE)
 
-    all_samples <- NULL
-  } else {
-    run_skippable_function(build_samples_tables,        feather_file_folder)
-  }
+  run_skippable_function(build_samples_table, all_samples_with_patient_ids)
+
+  # read-back sample db data ---------------------------------------------------
+  cat(crayon::magenta("Read the samples table to get samples data with ids."), fill = TRUE)
+  samples <- iatlas.data::read_table("samples") %>% dplyr::as_tibble()
+  cat(crayon::blue("Done reading the samples table."), fill = TRUE, sep = " ")
+
+  run_skippable_function(build_samples_to_tags_table,     all_samples_with_patient_ids, samples)
+  run_skippable_function(build_features_to_samples_table, all_samples_with_patient_ids, samples)
+  run_skippable_function(build_genes_to_samples_table,    all_samples_with_patient_ids, rna_seq_expr_matrix, genes, samples)
+
+  run_skippable_function(build_slides_table,              feather_file_folder, patients)
+
+  rm(samples)
+  rm(patients)
+  rm(all_samples)
+  rm(rna_seq_expr_matrix)
+  rm(genes)
+
+  # after build-samples-tables ---------------------------------------------------
   run_skippable_function(build_driver_results_tables, feather_file_folder)
   run_skippable_function(build_nodes_tables,          feather_file_folder)
 
