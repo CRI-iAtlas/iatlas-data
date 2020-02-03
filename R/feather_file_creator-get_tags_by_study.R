0bb88125ffc89cfc168bfd5098cb1d4559a430ec
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-02-03 15:48:32 -0800

    Simply get all tcga tags in a single file.

--- R/feather_file_creator-get_tags_by_study.R
+++ R/feather_file_creator-get_tags_by_study.R
@@ -7,30 +7,19 @@ get_tags_by_study <- function() {
     cat(crayon::cyan(paste0(" - ", message)), fill = TRUE)
   }
 
-  get_tags <- function(study) {
+  get_tags <- function() {
     current_pool <- pool::poolCheckout(.GlobalEnv$pool)
 
-    cat(crayon::magenta(paste0("Get tags by `", study, "`")), fill = TRUE)
+    cat(crayon::magenta(paste0("Get tags.")), fill = TRUE)
 
     cat_tags_status("Get initial data from the tags table.")
     tags <- current_pool %>% dplyr::tbl("tags")
 
-    cat_tags_status(paste0("Get all related tag ids for each tag in the table.\n",
-    "    - Then get all the related tags from the tags table.\n",
-    "    - Finally, filter down the tags to only tags related to the passed study."))
-    tags <- tags %>% dplyr::right_join(
-      current_pool %>%
-        dplyr::tbl("tags_to_tags") %>%
-        dplyr::right_join(
-          current_pool %>% dplyr::tbl("tags") %>%
-            dplyr::select(id, related_tag_name = name),
-          by = c("related_tag_id" = "id")) %>%
-        dplyr::filter(related_tag_name == study),
-      by = c("id" = "tag_id")
-    )
-
     cat_tags_status("Clean up the data set.")
-    tags <- tags %>% dplyr::distinct(name, characteristics, display, color)
+    tags <- tags %>%
+      dplyr::filter(!is.na(name)) %>%
+      dplyr::distinct(name, characteristics, display, color) %>%
+      dplyr::arrange(name)
 
     cat_tags_status("Execute the query and return a tibble.")
     tags <- tags %>% dplyr::as_tibble()
@@ -40,18 +29,8 @@ get_tags_by_study <- function() {
     return(tags)
   }
 
-  # Setting these to the GlobalEnv just for development purposes.
-  .GlobalEnv$tcga_study_tags <- "TCGA_Study" %>%
-    get_tags %>%
-    feather::write_feather(paste0(getwd(), "/feather_files/tags/tcga_study_tags.feather"))
-
-  .GlobalEnv$tcga_subtype_tags <- "TCGA_Subtype" %>%
-    get_tags %>%
-    feather::write_feather(paste0(getwd(), "/feather_files/tags/tcga_subtype_tags.feather"))
-
-  .GlobalEnv$immune_subtype_tags <- "Immune_Subtype" %>%
-    get_tags %>%
-    feather::write_feather(paste0(getwd(), "/feather_files/tags/immune_subtype_tags.feather"))
+  .GlobalEnv$tcga_tags <- get_tags() %>%
+    feather::write_feather(paste0(getwd(), "/feather_files/tags/tcga_tags.feather"))
 
   # Close the database connection.
   pool::poolClose(.GlobalEnv$pool)
@@ -60,9 +39,7 @@ get_tags_by_study <- function() {
   ### Clean up ###
   # Data
   rm(pool, pos = ".GlobalEnv")
-  rm(tcga_study_tags, pos = ".GlobalEnv")
-  rm(tcga_subtype_tags, pos = ".GlobalEnv")
-  rm(immune_subtype_tags, pos = ".GlobalEnv")
+  # rm(tcga_tags, pos = ".GlobalEnv")
   cat("Cleaned up.", fill = TRUE)
   gc()
 }
