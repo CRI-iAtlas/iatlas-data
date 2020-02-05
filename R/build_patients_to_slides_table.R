6a0eb41899e26dfe3c69830475400b8af5dd2adf
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-02-05 15:53:42 -0800

    Fixed column names to the actual column names.

--- R/build_patients_to_slides_table.R
+++ R/build_patients_to_slides_table.R
@@ -7,20 +7,25 @@ build_patients_to_slides_table <- function() {
     get_feather_file_folder(),
     "relationships/patients_to_slides"
   ) %>%
-    dplyr::distinct(patient_id, slide_id) %>%
-    dplyr::filter(!is.na(patient_id) & !is.na(slide_id)) %>%
-    dplyr::arrange(patient_id, slide_id)
+    dplyr::distinct(barcode, slide) %>%
+    dplyr::filter(!is.na(barcode) & !is.na(slide)) %>%
+    dplyr::arrange(barcode, slide)
   cat(crayon::blue("Imported feather files for patients_to_slides."), fill = TRUE)
 
   # patients_to_slides data ---------------------------------------------------
   cat(crayon::magenta("Building patients_to_slides data."), fill = TRUE)
   patients_to_slides <- patients_to_slides %>% dplyr::left_join(
     iatlas.data::read_table("slides") %>%
-      dplyr::select(slide_id = id, name),
-    by = "slide_id"
+      dplyr::select(slide_id = id, slide = name),
+    by = "slide"
   )
 
-  patients_to_slides <- patients_to_slides %>% dplyr::left_join(get_patients(), by = "patient_id")
+  patients_to_slides <- patients_to_slides %>%
+    dplyr::left_join(get_patients(), by = "barcode")
+
+  # .GlobalEnv$p_2_s <- cbind(patients_to_slides)
+
+  patients_to_slides <- patients_to_slides %>% dplyr::select(patient_id, slide_id)
   cat(crayon::blue("Build patients_to_slides data."), fill = TRUE)
 
   # patients_to_slides table ---------------------------------------------------
