fb81f31a9f582699f3e37addc2063c7ce0052de7
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-02-05 15:41:54 -0800

    No need to arrange retrieved samples as they will be arranged later anyway.

--- R/load_all_samples.R
+++ R/load_all_samples.R
@@ -1,5 +1,5 @@
 load_all_samples <- function() {
   cat(crayon::magenta("Importing feather files for samples."), fill = TRUE)
   on.exit(cat(crayon::blue("Imported feather files for samples."), fill = TRUE))
-  return(read_iatlas_data_file(get_feather_file_folder(), "samples") %>% dplyr::arrange(sample))
+  return(read_iatlas_data_file(get_feather_file_folder(), "samples"))
 }
