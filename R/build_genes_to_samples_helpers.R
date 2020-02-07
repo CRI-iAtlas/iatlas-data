0177b63c85405b84193553eae48e45cb0eead390
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-02-07 09:18:26 -0800

    REFACTOR - result_Cache

--- R/build_genes_to_samples_helpers.R
+++ R/build_genes_to_samples_helpers.R
@@ -1,23 +1,3 @@
-
-get_result_cache <- function () {
-  if (!present(.GlobalEnv$result_cache))
-    .GlobalEnv$result_cache <- new.env()
-  .GlobalEnv$result_cache
-}
-
-result_cached <- function (key, value) {
-  result_cache <- get_result_cache()
-  if (present(result_cache[[key]])) result_cache[[key]]
-  else result_cache[[key]] <- value
-}
-
-reset_results_cache <- function () {
-  if (present(.GlobalEnv$result_cache)) {
-    rm(result_cache, pos = .GlobalEnv)
-  }
-  gc()
-}
-
 set_feather_file_folder <- function(feather_file_folder) .GlobalEnv$feather_file_folder <- feather_file_folder
 get_feather_file_folder <- function() .GlobalEnv$feather_file_folder
 
