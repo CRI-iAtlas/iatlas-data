556dff854cc57e309318d885c046f85223e46fcd
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-02-07 10:14:04 -0800

    TEST - expanded result_cache test

--- tests/testthat/test_result_cache.R
+++ tests/testthat/test_result_cache.R
@@ -11,8 +11,13 @@
     reset_results_cache()
     a <- new.env()
     a$foo = 123
+
     result_cached("foo", 123)
     expect_equal(get_result_cache(), a)
+
+    reset_results_cache()
+    empty <- new.env()
+    expect_equal(get_result_cache(), empty)
   })
 
   test_that("expression is not executed if already cached", {
