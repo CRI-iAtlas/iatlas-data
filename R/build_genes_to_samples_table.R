83766915d3a8412fa7b119769d3efbbef412ea57
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-02-10 14:51:39 -0800

    Use resolve_df_dupes when building genes_to_samples table.

--- R/build_genes_to_samples_table.R
+++ R/build_genes_to_samples_table.R
@@ -2,15 +2,28 @@ build_genes_to_samples_table <- function() {
 
   # genes_to_samples import ---------------------------------------------------
   cat(crayon::magenta("Importing feather files for genes_to_samples."), fill = TRUE)
-  # TODO: This should be filtered by entrez not hgnc.
   genes_to_samples <- iatlas.data::read_iatlas_data_file(
-    get_feather_file_folder(),
+    iatlas.data::get_feather_file_folder(),
     "relationships/genes_to_samples"
-  ) %>%
-    dplyr::distinct(entrez, hgnc, sample, rna_seq_expr) %>%
-    dplyr::filter(!is.na(hgnc) & !is.na(sample)) %>%
-    dplyr::arrange(entrez, hgnc, sample, rna_seq_expr)
+  )
   cat(crayon::blue("Imported feather files for genes_to_samples."), fill = TRUE)
+
+  # genes_to_samples column fix ---------------------------------------------------
+  cat(crayon::magenta("Ensuring genes_to_samples have all the correct columns and no dupes."), fill = TRUE)
+  # TODO: This should be filtered by entrez not hgnc.
+  genes_to_samples <- genes_to_samples %>%
+    dplyr::bind_rows(dplyr::tibble(
+      entrez = integer(),
+      hgnc = character(),
+      sample = character(),
+      rna_seq_expr = numeric()
+    )) %>%
+    dplyr::distinct(entrez, hgnc, sample, rna_seq_expr) %>%
+    dplyr::filter((!is.na(entrez) | !is.na(hgnc)) & !is.na(sample)) %>%
+    iatlas.data::resolve_df_dupes(keys = c("hgnc", "sample")) %>%
+    dplyr::select(entrez, hgnc, sample, rna_seq_expr) %>%
+    dplyr::arrange(entrez, hgnc, sample)
+  cat(crayon::blue("Ensured genes_to_samples have all the correct columns and no dupes."), fill = TRUE)
 
   # genes_to_samples data ---------------------------------------------------
   cat(crayon::magenta("Building genes_to_samples data.\n\t(These are some large datasets, please be patient as they are read and built.)"), fill = TRUE)
