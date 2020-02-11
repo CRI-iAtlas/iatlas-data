c6379692b5a93caa183b35266adff5ba8737aa3d
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-02-11 15:48:05 -0800

    Differentiating between genes and features in nodes when getting data from original SQLite data.

--- R/old_build_nodes_tables.R
+++ R/old_build_nodes_tables.R
@@ -1,31 +1,45 @@
 old_build_nodes_tables <- function() {
 
   cat(crayon::magenta("Importing feather files for nodes."), fill = TRUE)
-  nodes <- iatlas.data::read_iatlas_data_file(iatlas.data::get_feather_file_folder(), "SQLite_data/nodes") %>%
-    dplyr::distinct(hgnc = Node, tag.01 = Group, tag.02 = Immune, score = UpBinRatio) %>%
-    dplyr::arrange(hgnc, tag.01, tag.02, score)
+  all_nodes <- iatlas.data::read_iatlas_data_file(iatlas.data::get_feather_file_folder(), "SQLite_data/nodes") %>%
+    dplyr::distinct(node = Node, tag.01 = Group, tag.02 = Immune, score = UpBinRatio) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(node = ifelse(
+      identical(node, "B_cells") |
+        identical(node, "Dendritic_cells") |
+        identical(node, "Eosinophils") |
+        identical(node, "Macrophage") |
+        identical(node, "Mast_cells") |
+        identical(node, "Neutrophils") |
+        identical(node, "NK_cells") |
+        identical(node, "T_cells_CD4") |
+        identical(node, "T_cells_CD8"),
+        paste0(node, ".Aggregate2"),
+        node
+    )) %>%
+    dplyr::arrange(node, tag.01, tag.02, score)
   cat(crayon::blue("Imported feather files for nodes."), fill = TRUE)
 
   cat(crayon::magenta("Building the nodes data."), fill = TRUE)
-  gene_nodes <- all_nodes %>%
-    dplyr::left_join(
-      iatlas.data::old_read_genes() %>%
-        dplyr::select(gene_id = id, hgnc),
-      by = "hgnc"
-    )
+  gene_nodes <- all_nodes %>% dplyr::left_join(
+    iatlas.data::old_read_genes() %>%
+      dplyr::select(gene_id = id, node = hgnc),
+    by = "node"
+  ) %>%
+    dplyr::filter(!is.na(gene_id))
 
-  feature_nodes <- all_nodes %>%
-    dplyr::left_join(
-      iatlas.data::read_table("features") %>%
-        dplyr::select(feature_id = id, hgnc = name) %>%
-        dplyr::as_tibble(),
-      by = "hgnc"
-    )
+  feature_nodes <- all_nodes %>% dplyr::left_join(
+    iatlas.data::old_read_features() %>%
+      dplyr::select(feature_id = id, node = name),
+    by = "node"
+  ) %>%
+    dplyr::filter(!is.na(feature_id))
 
-  nodes <- gene_nodes %>%
-    dplyr::bind_rows(feature_nodes)
+  nodes <- gene_nodes %>% dplyr::bind_rows(feature_nodes)
+
+  nodes <- nodes %>%
     dplyr::arrange(gene_id, feature_id, tag.01, tag.02, score) %>%
-    tibble::add_column(node_id = 1:nrow(nodes), .before = "gene_id")
+    tibble::add_column(node_id = 1:nrow(nodes), .before = "node")
   cat(crayon::blue("Built the nodes data."), fill = TRUE)
 
   cat(crayon::magenta("Building the nodes table."), fill = TRUE)
@@ -62,36 +76,47 @@ old_build_nodes_tables <- function() {
   cat(crayon::magenta("Importing feather files for edges."), fill = TRUE)
   edges <- iatlas.data::read_iatlas_data_file(iatlas.data::get_feather_file_folder(), "SQLite_data/edges") %>%
     dplyr::distinct(From, To, tag.01 = Group, tag.02 = Immune, score = ratioScore) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+      From = ifelse(
+        identical(From, "B_cells") |
+          identical(From, "Dendritic_cells") |
+          identical(From, "Eosinophils") |
+          identical(From, "Macrophage") |
+          identical(From, "Mast_cells") |
+          identical(From, "Neutrophils") |
+          identical(From, "NK_cells") |
+          identical(From, "T_cells_CD4") |
+          identical(From, "T_cells_CD8"),
+        paste0(From, ".Aggregate2"),
+        From
+      ),
+      To = ifelse(
+        identical(To, "B_cells") |
+          identical(To, "Dendritic_cells") |
+          identical(To, "Eosinophils") |
+          identical(To, "Macrophage") |
+          identical(To, "Mast_cells") |
+          identical(To, "Neutrophils") |
+          identical(To, "NK_cells") |
+          identical(To, "T_cells_CD4") |
+          identical(To, "T_cells_CD8"),
+        paste0(To, ".Aggregate2"),
+        To
+      )
+    ) %>%
     dplyr::arrange(From, To, tag.01, tag.02)
   cat(crayon::blue("Imported feather files for edges."), fill = TRUE)
 
   cat(crayon::magenta("Building the edges data."), fill = TRUE)
   edges <- edges %>% dplyr::left_join(
-    nodes %>%
-      dplyr::filter(is.na(feature_id)) %>%
-      dplyr::select(node_1_id = node_id, hgnc, tag.01, tag.02),
-    by = c("From" = "hgnc", "tag.01" = "tag.01", "tag.02" = "tag.02")
+    nodes %>% dplyr::select(node_1_id = node_id, node, tag.01, tag.02),
+    by = c("From" = "node", "tag.01" = "tag.01", "tag.02" = "tag.02")
   )
 
   edges <- edges %>% dplyr::left_join(
-    nodes %>%
-      dplyr::filter(is.na(gene_id)) %>%
-      dplyr::select(node_1_id = node_id, hgnc, tag.01, tag.02),
-    by = c("From" = "hgnc", "tag.01" = "tag.01", "tag.02" = "tag.02")
-  )
-
-  edges <- edges %>% dplyr::left_join(
-    nodes %>%
-      dplyr::filter(is.na(feature_id)) %>%
-      dplyr::select(node_2_id = node_id, hgnc, tag.01, tag.02),
-    by = c("To" = "hgnc", "tag.01" = "tag.01", "tag.02" = "tag.02")
-  )
-
-  edges <- edges %>% dplyr::left_join(
-    nodes %>%
-      dplyr::filter(is.na(gene_id)) %>%
-      dplyr::select(node_2_id = node_id, hgnc, tag.01, tag.02),
-    by = c("To" = "hgnc", "tag.01" = "tag.01", "tag.02" = "tag.02")
+    nodes %>% dplyr::select(node_2_id = node_id, node, tag.01, tag.02),
+    by = c("To" = "node", "tag.01" = "tag.01", "tag.02" = "tag.02")
   )
 
   edges <- edges %>%
