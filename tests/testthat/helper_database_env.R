57cb77fce0ec1297b782b1cac8dd23b9817cd7b7
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-02-12 15:11:39 -0800

    TESTS passing

--- tests/testthat/helper_database_env.R
+++ tests/testthat/helper_database_env.R
@@ -2,5 +2,5 @@ with_test_db_env <- function(expr) {
   load_config('test')
   cat(crayon::yellow("with_test_db_env",.GlobalEnv$DB_NAME,"\n"))
   on.exit(load_config('dev'))
-  expr
+  foo <- expr
 }
