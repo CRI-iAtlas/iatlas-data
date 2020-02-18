f9a11941e4c2a59979fbd3d2d301babb3cd2f17d
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-02-18 14:55:00 -0800

    TEST improved - devtools::check() passes (covr still fails)

--- init.R
+++ init.R
@@ -1,13 +1,4 @@
-load_config <- function(configName = "dev") {
-  dbConfig = config::get("database", config = configName)
-  .GlobalEnv$DB_NAME <- Sys.getenv("DB_NAME",  unset = dbConfig$name)
-  .GlobalEnv$DB_HOST <- Sys.getenv("DB_HOST",  unset = dbConfig$host)
-  .GlobalEnv$DB_PORT <- Sys.getenv("DB_PORT",  unset = dbConfig$port)
-  .GlobalEnv$DB_USER <- Sys.getenv("DB_USER",  unset = dbConfig$user)
-  .GlobalEnv$DB_PW   <- Sys.getenv("DB_PW",    unset = dbConfig$password)
-  cat(crayon::blue("Database config loaded:", configName, "\n"))
-}
-
+require('R/load_config.R')
 load_config(Sys.getenv("R_CONFIG_ACTIVE", unset = "dev"))
 
 devtools::load_all(devtools::as.package(".")$path)
