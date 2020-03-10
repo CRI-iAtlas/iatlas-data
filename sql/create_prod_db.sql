3070d360f7b7106bde7a0cb55d0ff5b10804f1a8
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-10 12:55:50 -0700

    SQL script to create prod DB.

---
+++ sql/create_prod_db.sql
@@ -0,0 +1,13 @@
+-- Terminate activity on DB before dropping it. See https://stackoverflow.com/questions/7073773/drop-postgresql-database-through-command-line
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='iatlas';
+-- Drop the dev database if it already exists.
+DROP DATABASE IF EXISTS iatlas;
+
+-- Create the dev database (used for development).
+CREATE DATABASE iatlas;
+
+-- Connect to the database.
+\connect iatlas
+
+-- Include the common table building SQL.
+\i create_enums.sql
