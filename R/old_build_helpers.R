99ae2ba6774b0d56fcd0970ed43fdd987bf51673
andrewelamb <andrewelamb@gmail.com>
2020-03-13 16:28:16 -0700

    namespaced functions

--- R/old_build_helpers.R
+++ R/old_build_helpers.R
@@ -36,16 +36,16 @@ old_get_all_samples <- function() result_cached("all_samples", old_load_all_samp
 old_get_all_samples_with_patient_ids <- function() result_cached("all_samples_with_patient_ids", old_get_all_samples() %>% dplyr::left_join(old_read_patients(), by = c("sample" = "barcode")))
 
 old_get_all_pcawg_samples_synapse <- function(){
-  create_global_synapse_connection()
-  result_cached("all_pcawg_samples_synapse", pcawg_synapse_id %>% .GlobalEnv$synapse$get() %>% .$path %>%  read.csv(sep = "\t", stringsAsFactors = F))
+  iatlas.data::create_global_synapse_connection()
+  iatlas.data::result_cached("all_pcawg_samples_synapse", pcawg_synapse_id %>% .GlobalEnv$synapse$get() %>% .$path %>%  read.csv(sep = "\t", stringsAsFactors = F))
 }
 
 old_get_tcga_samples_synapse <- function(){
-  create_global_synapse_connection()
-  result_cached("tcga_sample_ids", tcga_sample_synpse_id %>% .GlobalEnv$synapse$get() %>% .$path %>% read.csv(sep = "\t", stringsAsFactors = F) %>%  dplyr::pull(icgc_sample_id))
+  iatlas.data::create_global_synapse_connection()
+  iatlas.data::result_cached("tcga_sample_ids", tcga_sample_synpse_id %>% .GlobalEnv$synapse$get() %>% .$path %>% read.csv(sep = "\t", stringsAsFactors = F) %>%  dplyr::pull(icgc_sample_id))
 }
 
 old_get_pcawg_samples_synapse <- function(){
-  create_global_synapse_connection()
-  result_cached("tcga_pcawg_samples_synapse", get_pcawg_samples_synapse())
+  iatlas.data::create_global_synapse_connection()
+  iatlas.data::result_cached("tcga_pcawg_samples_synapse", iatlas.data::old_get_pcawg_samples_synapse())
 }
