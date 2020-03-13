21fb92f0b52af8f6f4419fd97abee58ef3fa88ae
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-13 15:31:47 -0700

    Updated synapse login functions.

--- R/synapse_functions.R
+++ R/synapse_functions.R
@@ -1,18 +1,32 @@
-synapse_logout <- function(){
-  if (!iatlas.data::present(.GlobalEnv$synapse)) {
-    .GlobalEnv$synapse$logout()
-    rm(synapse, pos = .GlobalEnv)
+attempt_instantiate <- function() {
+  if (reticulate::py_module_available("synapseclient")) {
+    synapse <- reticulate::import("synapseclient")
+    return(synapse$Synapse())
   }
+  cat(crayon::red("synapseclient unavailable!\n"))
+  return(NULL)
 }
-
 
 create_global_synapse_connection <- function() {
   if (!iatlas.data::present(.GlobalEnv$synapse)) {
-    synapse <- reticulate::import("synapseclient")
-    syn <- synapse$Synapse()
-    syn$login()
-    .GlobalEnv$synapse <- syn
+    syn <- iatlas.data::attempt_instantiate()
+    if (!is.null(syn) & is.null(syn$username)) {
+      syn$login()
+      return(.GlobalEnv$synapse <- syn)
+    } else if (is.null(syn)) {
+      cat(crayon::green("NOT Logged into Synapse\n"))
+      return(NA)
+    }
   }
   cat(crayon::green("Logged into Synapse\n"))
   return(.GlobalEnv$synapse)
+}
+
+synapse_logout <- function() {
+  if (iatlas.data::present(.GlobalEnv$synapse)) {
+    .GlobalEnv$synapse$logout()
+    rm(synapse, pos = ".GlobalEnv")
+  }
+  cat(crayon::green("Logged out of Synapse\n"))
+  return(NULL)
 }
