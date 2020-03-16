66fb5e3f72ba669d371471670fd94f41328fdc18
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-16 15:59:59 -0700

    Build PCAWG tags feather files with all the tags.

--- R/feather_file_creator-pcawg_build_tags_files.R
+++ R/feather_file_creator-pcawg_build_tags_files.R
@@ -8,13 +8,17 @@ pcawg_build_tags_files <- function() {
 
     cat(crayon::magenta(paste0("Get PCAWG tags.")), fill = TRUE)
 
-    cat_tags_status("Build tags data.")
-    tags <- dplyr::tibble(
-      name = "PCAWG",
-      display = "PCAWG"
-    ) %>%
+    cat_tags_status("Get initial values from Synapse.")
+    tags <- iatlas.data::get_pcawg_tag_values_cached() %>%
+      dplyr::select(name = tag) %>%
+      dplyr::mutate(display = NA %>% as.character()) %>%
       dplyr::add_row(name = "PCAWG_Study", display = "PCAWG Study") %>%
       dplyr::add_row(name = "Immune_Subtype", display = "Immune Subtype")
+
+    cat_tags_status("Clean up the data set.")
+    tags <- tags %>%
+      dplyr::distinct(name, display) %>%
+      dplyr::arrange(name)
 
     return(tags)
   }
