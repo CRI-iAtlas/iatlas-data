749e5887ca2585019467f32844dff0f769962aef
andrewelamb <andrewelamb@gmail.com>
2020-03-16 10:47:02 -0700

    add helpers for rnaseq

--- R/pcawg_helpers.R
+++ R/pcawg_helpers.R
@@ -1,6 +1,6 @@
 pcawg_synapse_id      <- "syn18234582"
 tcga_sample_synpse_id <- "syn18234560"
-pcawg_rna_synapse_id  <- "syn18134933"
+pcawg_rna_synapse_id  <- "syn18268621"
 
 get_all_pcawg_samples_synapse_cached <- function(){
   iatlas.data::create_global_synapse_connection()
@@ -28,7 +28,18 @@ get_tcga_samples_synapse_cached <- function(){
 get_pcawg_samples_synapse_cached <- function(){
   iatlas.data::create_global_synapse_connection()
   iatlas.data::result_cached(
-    "tcga_pcawg_samples_synapse",
+    "pcawg_samples",
     iatlas.data::get_pcawg_samples_synapse()
   )
 }
+
+get_pcawg_rnaseq_synapse_cached <- function(){
+  iatlas.data::create_global_synapse_connection()
+  iatlas.data::result_cached(
+    "pcawg_rnaseq",
+    iatlas.data::get_pcawg_rnaseq_synapse()
+  )
+}
+
+
+
