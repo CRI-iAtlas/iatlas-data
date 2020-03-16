d2982e3ce73301c525dd305b9843e3b45a7794ab
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-16 15:35:17 -0700

    Build PCAWG genes_to_samples feather files.

--- R/feather_file_creator-pcawg_build_genes_to_samples_files.R
+++ R/feather_file_creator-pcawg_build_genes_to_samples_files.R
@@ -7,10 +7,12 @@ pcawg_build_genes_to_samples_files <- function() {
   get_genes_to_samples <- function() {
 
     cat(crayon::magenta(paste0("Get PCAWG genes_to_samples.")), fill = TRUE)
-    rnaseq_synapse <- iatlas.data::get_pcawg_rnaseq_synapse_cached()
-    genes_to_samples <- rnaseq_synapse
 
     cat_genes_to_samples_status("Get the initial values from the genes_to_samples table.")
+    genes_to_samples <- iatlas.data::get_pcawg_rnaseq_synapse_cached()
+
+    cat_genes_to_samples_status("Clean up the data set.")
+    genes_to_samples <- genes_to_samples %>% dplyr::distinct(entrez, sample, rna_seq_expr)
 
     return(genes_to_samples)
   }
