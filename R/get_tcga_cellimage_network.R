a2934a6ccbd0e4e85f62b380604322a19589aedd
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-18 16:46:22 -0700

    Nodes and edges need to return th entrez column.

--- R/get_tcga_cellimage_network.R
+++ R/get_tcga_cellimage_network.R
@@ -1,27 +1,21 @@
+cellimage_cells <- c(
+  "B_cells",
+  "Dendritic_cells",
+  "Eosinophils",
+  "Macrophage",
+  "Mast_cells",
+  "NK_cells",
+  "Neutrophils",
+  "T_cells_CD4",
+  "T_cells_CD8"
+)
+
 get_tcga_cellimage_nodes <- function(){
   position_tbl <- iatlas.data::synapse_feather_id_to_tbl("syn21781366")
   nodes_tbl <- get_tcga_cytokine_nodes_cached()
-  gene_ids <- iatlas.data::get_gene_ids() %>%
-    tidyr::drop_na() %>%
-    dplyr::group_by(hgnc) %>%
-    dplyr::arrange(entrez) %>%
-    dplyr::slice(1) %>%
-    dplyr::ungroup()
-
-  cells <- c(
-    "B_cells",
-    "Dendritic_cells",
-    "Eosinophils",
-    "Macrophage",
-    "Mast_cells",
-    "NK_cells",
-    "Neutrophils",
-    "T_cells_CD4",
-    "T_cells_CD8"
-  )
 
   cellimage_nodes <- "syn21782167" %>%
-    iatlas.data::synapse_feather_id_to_tbl(.) %>%
+    iatlas.data::synapse_feather_id_to_tbl() %>%
     dplyr::select(From, To) %>%
     tidyr::pivot_longer(
       .,
@@ -32,12 +26,12 @@ get_tcga_cellimage_nodes <- function(){
     dplyr::select(node) %>%
     dplyr::distinct() %>%
     dplyr::full_join(position_tbl, by = c(node = "Variable")) %>%
-    dplyr::left_join(gene_ids, by = c("node" = "hgnc")) %>%
+    dplyr::left_join(iatlas.data::get_gene_ids(), by = c("node" = "hgnc")) %>%
     dplyr::mutate(node = dplyr::if_else(
       !is.na(entrez),
       as.character(entrez),
       dplyr::if_else(
-        node %in% cells,
+        node %in% cellimage_cells,
         paste0(node, "_Aggregate2"),
         dplyr::if_else(
           node == "Tumor_cell",
@@ -48,46 +42,28 @@ get_tcga_cellimage_nodes <- function(){
     )) %>%
     dplyr::select(-entrez)
 
-  nodes_tbl1 <- nodes_tbl %>%
-    dplyr::mutate(gene = as.character(gene)) %>%
-    dplyr::inner_join(cellimage_nodes, by = c("gene" = "node"))
+  gene_nodes <- nodes_tbl %>%
+    dplyr::mutate(entrez = as.character(entrez)) %>%
+    dplyr::inner_join(cellimage_nodes, by = c("entrez" = "node"))
 
-  nodes_tbl2 <- nodes_tbl %>%
+  feature_nodes <- nodes_tbl %>%
     dplyr::inner_join(cellimage_nodes, by = c("feature" = "node"))
 
-  dplyr::bind_rows(nodes_tbl1, nodes_tbl2)
+  gene_nodes %>% dplyr::bind_rows(feature_nodes)
 }
 
 get_tcga_cellimage_edges <- function(){
   edges_tbl <- get_tcga_cytokine_edges_cached()
-  gene_ids <- iatlas.data::get_gene_ids() %>%
-    tidyr::drop_na() %>%
-    dplyr::group_by(hgnc) %>%
-    dplyr::arrange(entrez) %>%
-    dplyr::slice(1) %>%
-    dplyr::ungroup()
-
-  cells <- c(
-    "B_cells",
-    "Dendritic_cells",
-    "Eosinophils",
-    "Macrophage",
-    "Mast_cells",
-    "NK_cells",
-    "Neutrophils",
-    "T_cells_CD4",
-    "T_cells_CD8"
-  )
 
   cellimage_edges <- "syn21782167" %>%
-    iatlas.data::synapse_feather_id_to_tbl(.) %>%
+    iatlas.data::synapse_feather_id_to_tbl() %>%
     dplyr::select(from = From, to = To, label = interaction) %>%
-    dplyr::left_join(gene_ids, by = c("from" = "hgnc")) %>%
+    dplyr::left_join(iatlas.data::get_gene_ids(), by = c("from" = "hgnc")) %>%
     dplyr::mutate(from = dplyr::if_else(
       !is.na(entrez),
       as.character(entrez),
       dplyr::if_else(
-        from %in% cells,
+        from %in% cellimage_cells,
         paste0(from, "_Aggregate2"),
         dplyr::if_else(
           from == "Tumor_cell",
@@ -97,12 +73,12 @@ get_tcga_cellimage_edges <- function(){
       )
     )) %>%
     dplyr::select(-entrez) %>%
-    dplyr::left_join(gene_ids, by = c("to" = "hgnc")) %>%
+    dplyr::left_join(iatlas.data::get_gene_ids(), by = c("to" = "hgnc")) %>%
     dplyr::mutate(to = dplyr::if_else(
       !is.na(entrez),
       as.character(entrez),
       dplyr::if_else(
-        to %in% cells,
+        to %in% cellimage_cells,
         paste0(to, "_Aggregate2"),
         dplyr::if_else(
           to == "Tumor_cell",
