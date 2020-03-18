bd2398282e8cf33be9dec302dbe570c59f1e8986
andrewelamb <andrewelamb@gmail.com>
2020-03-18 09:43:07 -0700

    tcga edges table helpers now working

--- R/get_tcga_cytokine_network.R
+++ R/get_tcga_cytokine_network.R
@@ -1,17 +1,6 @@
 get_tcga_cytokine_nodes <- function(){
   gene_ids <- feather::read_feather("feather_files/gene_ids.feather")
 
-  node_tbl <-
-    c("syn21781358", "syn21781359", "syn21781360", "syn21781362") %>%
-    purrr::map(synapse_feather_id_to_tbl) %>%
-    dplyr::bind_rows() %>%
-    dplyr::select(
-      node  = Node,
-      tag   = Group,
-      score = UpBinRatio,
-      tag.2 = Immune
-    )
-
   cells <- c(
     "B_cells",
     "Dendritic_cells",
@@ -23,6 +12,17 @@ get_tcga_cytokine_nodes <- function(){
     "T_cells_CD4",
     "T_cells_CD8"
   )
+
+  node_tbl <-
+    c("syn21781358", "syn21781359", "syn21781360", "syn21781362") %>%
+    purrr::map(synapse_feather_id_to_tbl) %>%
+    dplyr::bind_rows() %>%
+    dplyr::select(
+      node  = Node,
+      tag   = Group,
+      score = UpBinRatio,
+      tag.2 = Immune
+    )
 
   feature_node_tbl <- node_tbl %>%
     dplyr::filter(node %in% cells) %>%
@@ -52,6 +52,25 @@ get_tcga_cytokine_nodes <- function(){
 }
 
 get_tcga_cytokine_edges <- function(){
+  gene_ids <- feather::read_feather("feather_files/gene_ids.feather") %>%
+    tidyr::drop_na() %>%
+    dplyr::group_by(hgnc) %>%
+    dplyr::arrange(entrez) %>%
+    dplyr::slice(1) %>%
+    dplyr::ungroup()
+
+  cells <- c(
+    "B_cells",
+    "Dendritic_cells",
+    "Eosinophils",
+    "Macrophage",
+    "Mast_cells",
+    "NK_cells",
+    "Neutrophils",
+    "T_cells_CD4",
+    "T_cells_CD8"
+  )
+
   edge_tbl <-
     c("syn21781350", "syn21781351", "syn21781354", "syn21781357") %>%
     purrr::map(synapse_feather_id_to_tbl) %>%
@@ -62,9 +81,22 @@ get_tcga_cytokine_edges <- function(){
       score = ratioScore,
       tag   = Group,
       tag.2 = Immune
-    )
-
-
+    ) %>%
+    dplyr::left_join(gene_ids, by = c("from" = "hgnc")) %>%
+    dplyr::mutate(from = dplyr::if_else(
+      is.na(entrez),
+      paste0(from, "_Aggregate2"),
+      entrez
+    )) %>%
+    dplyr::select(-entrez) %>%
+    dplyr::left_join(gene_ids, by = c("to" = "hgnc")) %>%
+    dplyr::mutate(to = dplyr::if_else(
+      is.na(entrez),
+      paste0(to, "_Aggregate2"),
+      entrez
+    )) %>%
+    dplyr::select(-entrez) %>%
+    dplyr::filter(!is.na(tag))
 }
 
 synapse_feather_id_to_tbl <- function(id){
