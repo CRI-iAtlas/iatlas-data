f91442fd17f18e88468451429617e83ef7ea43f6
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-20 14:06:30 -0700

    If rna_seq_epr file is NOT already downloaded and converted to a feather file, download it from SYnapse and save it locally as a feather file for faster reading later.

--- R/load_rna_seq_expr.R
+++ R/load_rna_seq_expr.R
@@ -2,11 +2,29 @@ load_rna_seq_expr <- function(feather_file_folder, genes) {
   iatlas.data::timed(
     before_message = crayon::magenta("Importing HUGE RNA Seq Expr file.\n(This is VERY large and may take some time to open. Please be patient.)\n"),
     after_message = crayon::blue("Imported HUGE RNA Seq Expr file."),
-    iatlas.data::read_iatlas_data_file(feather_file_folder, "EBPlusPlusAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.feather") %>%
-      dplyr::as_tibble() %>%
-      tidyr::separate(gene_id, c("hugo", "entrez"), sep = "[|]") %>%
+
+    get_rna_seq_expr(feather_file_folder) %>%
+      tidyr::separate(gene_id, c("hgnc", "entrez"), sep = "[|]") %>%
       dplyr::select(-c(entrez)) %>%
-      dplyr::filter(hugo != "?") %>%
-      dplyr::filter(hugo %in% genes[["hgnc"]])
+      dplyr::filter(hgnc != "?") %>%
+      dplyr::filter(hgnc %in% genes[["hgnc"]])
   )
+}
+
+get_rna_seq_expr <- function(feather_file_folder) {
+  feather_file <- paste0(feather_file_folder, "/EBPlusPlusAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.feather")
+  if (!file.exists(feather_file)) {
+    cat(crayon::blue("Reading rn_seq_expr file from Synapse."), fill = TRUE)
+    iatlas.data::create_global_synapse_connection()
+    on.exit(iatlas.data::synapse_logout())
+    return(
+      "syn4976369" %>%
+        .GlobalEnv$synapse$get() %>%
+        .$path %>%
+        read.csv(stringsAsFactors = F, header = T, sep = "\t", check.names = F) %>%
+        dplyr::as_tibble() %>%
+        feather::write_feather(feather_file)
+    )
+  }
+  return(feather::read_feather(feather_file))
 }
