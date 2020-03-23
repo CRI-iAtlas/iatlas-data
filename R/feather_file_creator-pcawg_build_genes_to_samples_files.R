012a0abf53de6ec170a71491b170edef69be9e1d
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-23 08:35:48 -0700

    Fixed changed function name.

--- R/feather_file_creator-pcawg_build_genes_to_samples_files.R
+++ R/feather_file_creator-pcawg_build_genes_to_samples_files.R
@@ -9,7 +9,7 @@ pcawg_build_genes_to_samples_files <- function() {
     cat(crayon::magenta(paste0("Get PCAWG genes_to_samples.")), fill = TRUE)
 
     cat_genes_to_samples_status("Get the initial values from the genes_to_samples table.")
-    genes_to_samples <- iatlas.data::get_pcawg_rnaseq_synapse_cached()
+    genes_to_samples <- iatlas.data::get_pcawg_rnaseq_cached()
 
     cat_genes_to_samples_status("Clean up the data set.")
     genes_to_samples <- genes_to_samples %>% dplyr::distinct(entrez, sample, rna_seq_expr)
