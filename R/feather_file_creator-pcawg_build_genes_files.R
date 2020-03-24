0f2215f6ebfda70077cd666a965d009599cf2ed0
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-03-23 22:35:24 -0700

    Better build PCAWG genes.

--- R/feather_file_creator-pcawg_build_genes_files.R
+++ R/feather_file_creator-pcawg_build_genes_files.R
@@ -8,9 +8,15 @@ pcawg_build_genes_files <- function() {
 
     cat(crayon::magenta(paste0("Get PCAWG genes")), fill = TRUE)
 
+    cat_genes_status("Get human gene ids.")
+    human_gene_ids <- iatlas.data::get_human_gene_ids_cached()
+
     cat_genes_status("Get the inital values from Synapse.")
     genes <- iatlas.data::get_pcawg_rnaseq_cached() %>%
-      dplyr::select(entrez, hgnc)
+      dplyr::distinct(entrez, hgnc)
+
+    cat_genes_status("Ensure hgnc.")
+    genes <- genes %>% dplyr::mutate(hgnc = ifelse(entrez %in% human_gene_ids$entrez, NA, hgnc))
 
     cat_genes_status("Clean up the data set.")
     genes <- genes %>% dplyr::distinct(entrez, hgnc)
