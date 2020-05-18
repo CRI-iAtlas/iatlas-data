31b9ee3af12e3112b2ec182a0d2b1ee62d69b12d
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-05-18 12:02:16 -0700

    Created helper function for gettting known gene resolutions and resolving the conflicts.

--- R/data_migration_helpers.R
+++ R/data_migration_helpers.R
@@ -4,6 +4,19 @@ get_gene_ids <- function() {
     feather::read_feather("feather_files/gene_ids.feather") %>% dplyr::as_tibble())
 }
 
+get_known_gene_resolutions <- function() {
+  iatlas.data::result_cached(
+    "known_gene_resolutions",
+    feather::read_feather("feather_files/known_gene_resolutions.feather") %>% dplyr::as_tibble())
+}
+
+resolve_hgnc_conflicts <- function(genes) {
+  genes %>% dplyr::left_join(get_known_gene_resolutions(), by = c("entrez")) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(hgnc = ifelse(identical(hgnc, alias), official, hgnc)) %>%
+    dplyr::select(-c(alias, official))
+}
+
 synapse_feather_id_to_tbl <- function(id) {
   id %>%
     .GlobalEnv$synapse$get() %>%
