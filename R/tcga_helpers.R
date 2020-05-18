d4ec704ac392e8030b4a5e046a9689545abfaf8f
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-05-18 12:01:13 -0700

    Moved repeatable gene gathering into helper functions.

--- R/tcga_helpers.R
+++ R/tcga_helpers.R
@@ -37,3 +37,81 @@ get_tcga_copynumber_results_cached <- function() {
     iatlas.data::get_tcga_copynumber_results()
   )
 }
+
+get_tcga_driver_mutation_genes <- function() {
+  iatlas.data::result_cached(
+    "tcga_driver_mutation_genes",
+    iatlas.data::read_iatlas_data_file(
+      iatlas.data::get_feather_file_folder(),
+      "SQLite_data/driver_mutations*.feather"
+    ) %>%
+      dplyr::distinct(hgnc = gene) %>%
+      dplyr::arrange(hgnc)
+  )
+}
+
+get_tcga_immunodulator_genes <- function() {
+  iatlas.data::result_cached(
+    "tcga_immunodulator_genes",
+    iatlas.data::read_iatlas_data_file(
+      iatlas.data::get_feather_file_folder(),
+      "SQLite_data/immunomodulators.feather"
+    ) %>%
+      dplyr::filter(!is.na(gene)) %>%
+      dplyr::rename(friendly_name = display2) %>%
+      dplyr::rename(hgnc = gene) %>%
+      dplyr::mutate(references = iatlas.data::build_references(reference)) %>%
+      dplyr::select(-c("display", "entrez", "reference")) %>%
+      dplyr::arrange(hgnc)
+  )
+}
+
+get_tcga_io_target_genes <- function() {
+  iatlas.data::result_cached(
+    "tcga_io_target_genes",
+    iatlas.data::read_iatlas_data_file(
+      iatlas.data::get_feather_file_folder(),
+      "SQLite_data/io_targets.feather"
+    ) %>%
+      dplyr::filter(!is.na(gene)) %>%
+      dplyr::distinct(hgnc = gene, .keep_all = TRUE) %>%
+      dplyr::select(-c("entrez")) %>%
+      dplyr::rename(io_landscape_name = display2) %>%
+      dplyr::mutate(references = iatlas.data::link_to_references(link)) %>%
+      dplyr::select(-c("display", "link")) %>%
+      dplyr::arrange(hgnc)
+  )
+}
+
+get_tcga_io_target_expr_genes <- function() {
+  iatlas.data::result_cached(
+    "tcga_io_target_expr_genes",
+    iatlas.data::read_iatlas_data_file(
+      iatlas.data::get_feather_file_folder(),
+      "SQLite_data/io_target_expr*.feather"
+    ) %>%
+      dplyr::distinct(hgnc = gene) %>%
+      dplyr::arrange(hgnc)
+  )
+}
+
+get_tcga_immunomodulator_expr_genes <- function() {
+  iatlas.data::result_cached(
+    "tcga_immunomodulator_expr_genes",
+    iatlas.data::read_iatlas_data_file(
+      iatlas.data::get_feather_file_folder(),
+      "SQLite_data/immunomodulator_expr.feather"
+    ) %>%
+      dplyr::distinct(hgnc = gene) %>%
+      dplyr::arrange(hgnc)
+  )
+}
+
+get_tcga_ecn_genes <- function() {
+  iatlas.data::result_cached(
+    "tcga_ecn_genes",
+    iatlas.data::read_iatlas_data_file(iatlas.data::get_feather_file_folder(), "ecn_genes.feather") %>%
+      dplyr::select(-c("entrez")) %>%
+      dplyr::arrange(hgnc)
+  )
+}
