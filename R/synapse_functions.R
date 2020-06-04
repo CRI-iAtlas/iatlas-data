5b9aa78ede95abfc8d8577be21962991fa676094
andrewelamb <andrewelamb@gmail.com>
2020-06-04 08:55:50 -0700

    create connection now adds synapseclient object to global env

--- R/synapse_functions.R
+++ R/synapse_functions.R
@@ -1,22 +1,24 @@
 attempt_instantiate <- function() {
   if (reticulate::py_module_available("synapseclient")) {
     synapse <- reticulate::import("synapseclient")
-    return(synapse$Synapse())
+    return(synapse)
   }
   cat(crayon::red("synapseclient unavailable!\n"))
   return(NULL)
 }
 
 create_global_synapse_connection <- function() {
-  if (!iatlas.data::present(.GlobalEnv$synapse)) {
+  if (!iatlas.data::present(.GlobalEnv$syn)) {
     syn <- iatlas.data::attempt_instantiate()
-    if (!is.null(syn) & is.null(syn$username)) {
-      syn$login()
-    } else if (is.null(syn)) {
+    synapse <- syn$Synapse()
+    if (!is.null(synapse) & is.null(synapse$username)) {
+      synapse$login()
+    } else if (is.null(synapse)) {
       cat(crayon::green("NOT Logged into Synapse\n"))
       return(NA)
     }
-    .GlobalEnv$synapse <- syn
+    .GlobalEnv$syn <- syn
+    .GlobalEnv$synapse <- synapse
     cat(crayon::green("Logged into Synapse\n"))
   } else {
     cat(crayon::green("Already logged into Synapse\n"))
@@ -31,4 +33,9 @@ synapse_logout <- function() {
   }
   cat(crayon::green("Logged out of Synapse\n"))
   return(NULL)
+}
+
+synapse_store_file <- function(file, parent_id) {
+  file_entity <- .GlobalEnv$syn$File(file, parent_id)
+  .GlobalEnv$synapse$store(file_entity)
 }
